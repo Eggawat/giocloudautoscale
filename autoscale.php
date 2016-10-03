5b22a856fa59346359fcdbccec9046ad821c02da
biznetgiocloud <yoga_nugroho@Yogas-MacBook-Pro.local>
2016-10-03 13:53:32 +0700

    add resource

---
+++ autoscale.php
@@ -0,0 +1,64 @@
+<?php
+    //monitoring
+    $cpu = shell_exec("top -bn2 | awk '/Cpu\(s\):/ { print 100-$8 }'| awk 'NR==2'");
+    $memory = shell_exec("free | grep Mem | awk '{print $3/$2 * 100.0}'");
+
+    //parameter
+    $zoneid="db5dbd35-89c0-4df0-aba8-ff730c635a23";
+    $svcid="f98253c3-4a2c-40d5-b072-f00c2c545721";
+    $templateid="14abe807-79e2-481a-9c1d-387b8bb9ee49";
+    $networkid="5d066219-98a7-49e0-9bc9-3cda2295f413";
+    $region="jakarta";
+    $lbid="c98285e7-f876-44af-ad63-cae4b5e465ff";
+
+    //db connection
+    $con = mysqli_connect("localhost","root","biznetgio2016","autoscale");
+    if (mysqli_connect_errno()) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+
+    //query database
+    $query = "SELECT * FROM vm ORDER BY id DESC LIMIT 1";
+    $result = mysqli_query($con,$query);
+    $row=mysqli_fetch_assoc($result);
+    $id=$row['id'];
+    $vmid=$row['vmid'];
+    $vmclone= $row['id']+1;
+    $name="Web-$vmclone";
+
+    //condition
+    if ($memory > 40 || $cpu > 40){
+
+    //deploy vm
+        $deploy = shell_exec("cs deployVirtualMachine zoneid=$zoneid serviceofferingid=$svcid templateid=$templateid networkids=$networkid name=$name --region=$region 2>&1");
+
+    //get vm id
+    $getvmid = shell_exec("cs listVirtualMachines name='$name' --region=$region | grep -m 1 '\"id\":'|awk '{print substr($2,2,36)}'");
+    $getvmid = str_replace("\n", '', $getvmid);
+    $getvmid = str_replace('\n', '', $getvmid);
+
+    //get ip address
+    $getip = shell_exec("cs listVirtualMachines name='$name' --region=$region|grep ipaddress|awk '{print substr($2,2,16)}'");
+    $getip = str_replace('",', '', $getip);
+    $getip = str_replace("\n", '', $getip);
+    $getip = str_replace('\n', '', $getip);
+
+    //assign to lb
+    $tolb = shell_exec("cs assignToLoadBalancerRule id='$lbid' virtualmachineids='$getvmid' --region=$region 2>&1");
+
+    //insert database
+    mysqli_query($con,"INSERT INTO vm (vmname,vmid,ip) values('$name','$getvmid','$getip')");
+    mysqli_close($con);
+        } else {
+
+        if ($memory < 20 && $cpu < 20){
+            //destroy vm
+            $output = shell_exec("cs destroyVirtualMachine id=$vmid expunge=true --region=$region 2>&1");
+
+            //delete database
+            mysqli_query($con,"DELETE FROM vm WHERE vmid='$vmid'");
+            mysqli_close($con);
+            } else {
+        }
+}
+?>
---
+++ dashboard.php
@@ -0,0 +1,113 @@
+<?php
+    //monitoring
+    $cpu = shell_exec("top -bn2 | awk '/Cpu\(s\):/ { print 100-$8 }'| awk 'NR==2'");
+    $memory = shell_exec("free | grep Mem | awk '{print $3/$2 * 100.0}'");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <script src="js/jquery.js"></script>
+    <link   href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <div class="container">
+            <div class="row">
+                <h3>Autoscaling</h3>
+            </div>
+            <div class="row">
+                <table class="table table-striped table-bordered">
+                  <thead>
+                    <tr>
+                      <th>CPU Usage</th>
+                      <th>Memory Usage</th>
+                      <th>Last VM Created</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                  <?php
+                        $con = mysqli_connect("localhost","root","biznetgio2016","autoscale");
+                        // Check connection
+                        if (mysqli_connect_errno())
+                        {
+                        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+                        }
+
+                        // Perform queries
+                        $query = "SELECT * FROM vm ORDER BY id DESC LIMIT 1";
+                        $result = mysqli_query($con,$query);
+                        $row=mysqli_fetch_assoc($result);
+
+                            echo '<tr>';
+                            echo '<td>'. $cpu . '</td>';
+                            echo '<td>'. $memory . '</td>';
+                            echo '<td>'. $row['vmname'] . '</td>';
+                        //  echo '<td width=250>';
+                        //  echo '<a class="btn" href="details.php?id='.$row['id'].'">Details</a>';
+                            echo '</td>';
+                            echo '</tr>';
+
+                   mysqli_close($con);
+                  ?>
+                  </tbody>
+            </table>
+        </div>
+
+           <div class="row">
+                <h3>Current VM Running</h3>
+            </div>
+            <div class="row">
+                <table class="table table-striped table-bordered">
+                  <thead>
+                    <tr>
+                      <th>ID</th>
+                      <th>VM Name</th>
+                      <th>VM ID</th>
+                      <th>IP</th>
+                      <th>Status</th>
+                    </tr>
+                  </thead>
+
+                  <tbody>
+                  <?php
+                        $con = mysqli_connect("localhost","root","biznetgio2016","autoscale");
+                        // Check connection
+                        if (mysqli_connect_errno())
+                        {
+                        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+                        }
+                        // Perform queries
+                        $query = "SELECT * FROM vm ORDER BY id DESC";
+                        $result = mysqli_query($con,$query);
+
+                        if ($result->num_rows > 0){
+                            while ($row=$result->fetch_assoc()){
+                                $status = shell_exec("ping ".$row['ip']." -c 1 -W 1 |grep received|awk '{print substr($4,1,100)}'");
+                                echo '<tr>';
+                                echo '<td>'. $row['id'] .'</td>';
+                                echo '<td>'. $row['vmname'] .'</td>';
+                                echo '<td>'. $row['vmid'] .'</td>';
+                                echo '<td>'. $row['ip'] .'</td>';
+                                if ($status == 0){
+                                    echo '<td> NOT OK </td>';
+                                } else {
+                                    echo '<td> OK </td>';
+                                }
+                                echo '</td>';
+                                echo '</tr>';
+                            }
+                        } else {
+                        echo "0results";
+                        }
+                   mysqli_close($con);
+                  ?>
+                  </tbody>
+            </table>
+        </div>
+    </div> <!-- /container -->
+  </body>
+</html>
+
