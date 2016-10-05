541d45e989e983a47e0d724185bafbce1d88b283
Yoga Riyadi Nugroho <yoga.rnugroho@gmail.com>
2016-10-05 17:17:51 +0700

    Update autoscale.php

--- autoscale.php
+++ autoscale.php
@@ -6,14 +6,6 @@
     $cpu = shell_exec("top -bn2 | awk '/Cpu\(s\):/ { print 100-$8 }'| awk 'NR==2'");
     $memory = shell_exec("free | grep Mem | awk '{print $3/$2 * 100.0}'");
 
-    //parameter
-    $zoneid="db5dbd35-89c0-4df0-aba8-ff730c635a23";
-    $svcid="f98253c3-4a2c-40d5-b072-f00c2c545721";
-    $templateid="14abe807-79e2-481a-9c1d-387b8bb9ee49";
-    $networkid="5d066219-98a7-49e0-9bc9-3cda2295f413";
-    $region="jakarta";
-    $lbid="c98285e7-f876-44af-ad63-cae4b5e465ff";
-
     //db connection
     $con = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
     if (mysqli_connect_errno()) {
@@ -33,7 +25,7 @@
     if ($memory > 40 || $cpu > 40){
 
     //deploy vm
-        $deploy = shell_exec("cs deployVirtualMachine zoneid=$zoneid serviceofferingid=$svcid templateid=$templateid networkids=$networkid name=$name --region=$region 2>&1");
+    $deploy = shell_exec("cs deployVirtualMachine zoneid=$zoneid serviceofferingid=$svcid templateid=$templateid networkids=$networkid name=$name --region=$region 2>&1");
 
     //get vm id
     $getvmid = shell_exec("cs listVirtualMachines name='$name' --region=$region | grep -m 1 '\"id\":'|awk '{print substr($2,2,36)}'");
