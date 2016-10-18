0430131559b478a84f436d40cb4759a9b2eb5da5
Yoga Riyadi Nugroho <yoga.rnugroho@gmail.com>
2016-10-18 14:48:50 +0700

    Update config.php

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ define('zoneid','26def56d-bffe-47b8-8b94-10973879408b');
 //service offering id
 define('svcid','b21db01a-d1ad-4612-9b1f-e60127a7a5bb');
 //template id
-define('templateid','14abe807-79e2-481a-9c1d-387b8bb9ee49');
+define('templateid','574b6f09-f9ab-4795-9379-54802602201b');
 //network id
 define('networkid','2cb5d477-5f2e-46e9-9775-2a12a7031fec');
 //region jakarta/bali
