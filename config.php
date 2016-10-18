d682eec7d2329962b8dd9a0fb1d4ff5fdb7b3058
Yoga Riyadi Nugroho <yoga.rnugroho@gmail.com>
2016-10-18 14:31:00 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,17 @@
 <?php
 
 //vm config
-define('zoneid','db5dbd35-89c0-4df0-aba8-ff730c635a23');
-define('svcid','f98253c3-4a2c-40d5-b072-f00c2c545721');
+//zone id
+define('zoneid','14abe807-79e2-481a-9c1d-387b8bb9ee49');
+//service offering id
+define('svcid','14abe807-79e2-481a-9c1d-387b8bb9ee49');
+//template id
 define('templateid','14abe807-79e2-481a-9c1d-387b8bb9ee49');
+//network id
 define('networkid','5d066219-98a7-49e0-9bc9-3cda2295f413');
-define('region','jakarta');
+//region
+define('region',' ');
+//load balancer id 
 define('lbid','c98285e7-f876-44af-ad63-cae4b5e465ff');
 
 //db config
