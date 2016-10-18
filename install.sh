dd33cc48e4012483d7475869d6e95f36c58d0669
Yoga Riyadi Nugroho <yoga.rnugroho@gmail.com>
2016-10-18 15:25:07 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 yum -y install epel-release
 yum -y install git
 yum -y install httpd
-yum -y install php* --skip-broken
+yum -y install php --skip-broken
 yum -y install php-mysql
-yum -y install python* --skip-broken
+yum -y install python --skip-broken
 yum -y install mariadb-server mariadb
 yum -y install python-pip
