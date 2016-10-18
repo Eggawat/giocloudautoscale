deaf180c9054941eeccc78b4c368d5b20b01966f
Yoga Riyadi Nugroho <yoga.rnugroho@gmail.com>
2016-10-18 14:47:03 +0700

    Update config.php

--- config.php
+++ config.php
@@ -8,7 +8,7 @@ define('svcid','b21db01a-d1ad-4612-9b1f-e60127a7a5bb');
 //template id
 define('templateid','14abe807-79e2-481a-9c1d-387b8bb9ee49');
 //network id
-define('networkid','5d066219-98a7-49e0-9bc9-3cda2295f413');
+define('networkid','2cb5d477-5f2e-46e9-9775-2a12a7031fec');
 //region jakarta/bali
 define('region','bali');
 //load balancer id 
